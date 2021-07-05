7e3d21430768856226e3bef22a86bc15cb19963c
Steven Weingärtner <pd@frozen-bits.de>
2021-07-05 10:14:48 +0200

    fix: set correct base path for project

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ if (!password) throw new Error('Missing argument 4: Password');
 
 const fullPath = process.env.NODE_ENV === 'development'
     ? join('..', manifestPath)
-    : join('/github/workflow', manifestPath);
+    : join('/github/workspace', manifestPath);
 
 try {
     await createRelease(fullPath, packageId, username, password);
